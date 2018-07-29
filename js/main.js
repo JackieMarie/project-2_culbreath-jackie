6fc8c500ef25dd30d1f7c91775d1382c99771837
Jackie Culbreath <jmc081964@gmail.com>
2018-07-29 13:43:29 -0400

    trying to get modal to work, updating main.js and index.html

--- js/main.js
+++ js/main.js
@@ -22,3 +22,25 @@ console.log(nameInput);
   });
 
 })();
+
+// Modal
+
+var $modalTrigger = $('.modal__trigger-btn'),
+  $modal = $('.modal'),
+  $modalClose = $('.modal__close'),
+  $modalActive = '.modal--active';
+
+  $modalTrigger.on('click', function(e) {
+    e.preventDefault();
+    console.log('hello');
+
+    $(this).siblings($modal).addClass($modalActive);
+    $modal.attr('aria-hidden', 'false');
+  });
+
+  $modalClose.on('click', function(e) {
+    e.preventDefault();
+    console.log('I am closing');
+
+    $(this).parents($modal).removeClass($modalActive);
+  });
