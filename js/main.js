ca3a2727151d9ce61e6f5093dde709da235175af
Jackie Culbreath <jmc081964@gmail.com>
2018-08-04 10:27:09 -0400

    fixed modal

--- js/main.js
+++ js/main.js
@@ -1,46 +1,42 @@
 // To disable submit button on Contact Page
 
-var form = document.getElementById('form')
-var nameInput = document.getElementById('name');
-var telephoneInput = document.getElementById('telephone');
-var emailInput = document.getElementById('email');
-var contactmethodInput = document.getElementById('contactmethod');
-var messageInput = document.getElementById('message');
-var submit = document.getElementById('submit');
-var reset = document.getElementById('reset');
+$(function(){
+  $('.input').keyup(function() {
 
-console.log(nameInput);
+    var empty = false;
+    $('.input').each(function(){
+      if ($(this).val() == '') {
+        empty = true;
+      }
+    });
 
-(function() {
-  var submitted = false;
-  console.log(submit);
-  submit.disabled = true;
-  submit.className = "button--disabled";
-
-  addEvent(nameInput, 'input', function(e) {
-    console.log('hello')
+    if (empty) {
+      $('#submit').attr('disabled', 'disabled').hasClass('.button--disabled').removeClass('button--primary');
+    } else {
+      $('#submit').removeAttr('disabled').addClass('button--primary').removeClass('button--disabled');
+    }
   });
+})()
 
-})();
 
 // Modal
 
 var $modalTrigger = $('.modal__trigger-btn'),
-  $modal = $('.modal'),
+  $modalContainer = $('.modal__container'),
   $modalClose = $('.modal__close'),
-  $modalActive = '.modal--active';
+  $modalActive = 'modal--active';
 
 $modalTrigger.on('click', function(e) {
   e.preventDefault();
   console.log('hello');
 
-  $(this).siblings($modal).addClass($modalActive);
-  $modal.attr('aria-hidden', 'false');
+  $($modalContainer).addClass($modalActive);
+  $modalContainer.attr('aria-hidden', 'false');
 });
 
 $modalClose.on('click', function(e) {
   e.preventDefault();
   console.log('I am closing');
 
-  $(this).parents($modal).removeClass($modalActive);
+  $($modalContainer).removeClass($modalActive);
 });
